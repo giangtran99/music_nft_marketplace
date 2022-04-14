3bd532cbaa35f7c86fe1fd20890e81e26b848cb2
Tran Giang <hoanggiangg199@gmail.com>
2022-04-14 23:18:11 +0700

    update

--- src/components/Content/MintNFT/index.js
+++ src/components/Content/MintNFT/index.js
@@ -181,9 +181,9 @@ const MintForm = () => {
                           <svg xmlns="http://www.w3.org/2000/svg"
                             class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
                             fill="currentColor">
-                            <path fill-rule="evenodd"
+                            <path fillRule="evenodd"
                               d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                              clip-rule="evenodd" />
+                              clipRule="evenodd" />
                           </svg>
                           <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
                             Select a photo</p>
@@ -202,16 +202,16 @@ const MintForm = () => {
                       <label
                         class="flex flex-col w-full h-[300px] border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
                         <div class="relative flex flex-col items-center justify-center pt-16">
-                          {capturedFile.type === "audio" ? <audio controls autoplay>
+                          {capturedFile.type === "audio" ? <audio controls>
                             <source src={capturedFile.source} type="audio/ogg" />
                           </audio>
                             : <img src={capturedFile.source} id="preview" class="absolute inset-0 w-64 h-auto" />}
                           <svg xmlns="http://www.w3.org/2000/svg"
                             class="w-12 h-14 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
                             fill="currentColor">
-                            <path fill-rule="evenodd"
+                            <path fillRule="evenodd"
                               d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                              clip-rule="evenodd" />
+                              clipRule="evenodd" />
                           </svg>
                           <p class="pt-2 text-sm tracking-wider text-gray-400 group-hover:text-indigo-400"> Select a photo or audio</p>
                         </div>
--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -5,7 +5,7 @@ import MarketplaceContext from '../../store/marketplace-context';
 import web3 from '../../connection/web3';
 import { formatPrice } from '../../helpers/utils';
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { BellIcon, MenuIcon, XIcon,RefreshIcon } from '@heroicons/react/outline'
 
 
 const user = {
@@ -117,6 +117,14 @@ const Navbar = () => {
               </div>
               <div className="hidden md:block">
                 <div className="ml-4 flex items-center md:ml-6">
+                <button
+                    onClick={claimFundsHandler}
+                    type="button"
+                    className="mr-3 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  >
+                    <span className="sr-only">View notifications</span>
+                    <RefreshIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
                   <button
                     type="button"
                     className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
